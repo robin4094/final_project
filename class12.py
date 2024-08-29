55af2c1339476e641a5eda77372238ee2f34789c
robin4094 <hwangrb0310@gmail.com>
2024-08-29 16:51:05 -0400

    third commit

--- class12.py
+++ class12.py
@@ -1,2 +1,5 @@
-print("hello world")
-print("hello world2")
+print("welcome to the adventure game")
+print("당신은 현재 어두운 숲에 있습니다. 당신 앞에는 두 가지의 길이 있습니다")
+print("동굴로 들어가는 첫 번쨰길을 선택하고 싶으시면 'cave'라고 입력해주세요. 강으로 가고싶으시면 'river'를 입력해주세요")
+
+choice = input("당신의 선택: ")
